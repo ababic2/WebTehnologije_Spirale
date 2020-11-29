d71b7e2632068ca39928ebffcd78553b3a111765
Amina Babic <ababic2@etf.unsa.ba>
2020-11-29 11:50:38 +0100

    Validacija ukoliko nema kreiranog rasporeda

--- Zadatak1/iscrtaj.js
+++ Zadatak1/iscrtaj.js
@@ -199,7 +199,7 @@ function clearAllBordersInRow(row, cells) {
 }
 
 function dodajAktivnost(raspored, naziv, tip, vrijemePocetak, vrijemeKraj,dan) {
-    if(raspored.getElementsByTagName("table") == null) {
+    if(raspored.getElementsByTagName("table").length === 0) {
         window.alert("Greška-raspored nije kreiran")
     } else {
         let buildName = naziv.bold() + "<br />" + tip.toString();
@@ -306,13 +306,13 @@ function dodajAktivnost(raspored, naziv, tip, vrijemePocetak, vrijemeKraj,dan) {
 }
 
 let okvir = document.getElementById("okvir");
-iscrtajRaspored(okvir, ["Ponedjeljak","Utorak","Srijeda","Četvrtak","Petak"],8, 21);
+iscrtajRaspored(okvir, ["Ponedjeljak","Utorak","Srijeda","Četvrtak","Petak"],-1, 21);
 dodajAktivnost(okvir,"WT","predavanje",9,12,"Ponedjeljak");
-dodajAktivnost(okvir,"WT","vježbe",12,13.5,"Ponedjeljak");
-dodajAktivnost(okvir,"RMA","predavanje",14,17,"Ponedjeljak");
-dodajAktivnost(okvir,"RMA","KOKOLO",17.5,19.5,"Ponedjeljak");
-dodajAktivnost(okvir,"RMA","vježbe",12.5,14,"Utorak");
-dodajAktivnost(okvir,"DM","tutorijal",14,16,"Utorak");
-dodajAktivnost(okvir,"DM","predavanje",16,19,"Utorak");
+// dodajAktivnost(okvir,"WT","vježbe",12,13.5,"Ponedjeljak");
+// dodajAktivnost(okvir,"RMA","predavanje",14,17,"Ponedjeljak");
+// dodajAktivnost(okvir,"RMA","KOKOLO",17.5,19.5,"Ponedjeljak");
+// dodajAktivnost(okvir,"RMA","vježbe",12.5,14,"Utorak");
+// dodajAktivnost(okvir,"DM","tutorijal",14,16,"Utorak");
+// dodajAktivnost(okvir,"DM","predavanje",16,19,"Utorak");
 // dodajAktivnost(okvir,"OI","predavanje",12,15,"Ponedjeljak");
 
