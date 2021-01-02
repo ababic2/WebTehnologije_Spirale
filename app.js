1bd842200024eef377af6f03099c8c8821356753
Amina Babic <ababic2@etf.unsa.ba>
2021-01-02 21:15:31 +0100

    get aktivnosti vraca json niz

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const predmeti = require('./routes/predmeti');
+const aktivnosti = require('./routes/aktivnosti');
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,'public')));
 app.use('/predmeti',predmeti);
+app.use('/aktivnosti', aktivnosti);
 
 app.listen(3000);
---
+++ routes/aktivnosti.js
@@ -0,0 +1,35 @@
+// {naziv:string,tip:string,pocetak:number,kraj:number,dan:string}
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const router = express.Router();
+
+function readFile(res) {
+
+    function Aktivnost(naziv, tip, pocetak, kraj, dan) {
+        this.naziv = naziv;
+        this.tip = tip;
+        this.pocetak = pocetak;
+        this.kraj = kraj;
+        this.dan = dan;
+    }
+
+    let result = [];
+    let data = fs.readFileSync('aktivnosti.txt', 'utf8');
+
+    let splitted = data.toString().split("\n");
+    for (let i = 0; i<splitted.length; i++) {
+        let splitLine = splitted[i].split(",");
+
+        let aktivnost = new Aktivnost(splitLine[0], splitLine[1],splitLine[2], splitLine[3], splitLine[4]);
+        result.push(aktivnost);
+    }
+    console.log(result);
+    return result;
+}
+router.get('/', (req, res) => {
+    let result = readFile();
+    res.send(JSON.stringify(result));
+});
+
+module.exports = router;
