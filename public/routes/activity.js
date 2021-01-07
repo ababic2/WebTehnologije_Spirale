9591d804058bc576d03341eebc6a045322a578b8
Amina Babic <ababic2@etf.unsa.ba>
2021-01-07 13:36:21 +0100

    implementovano brisanje aktivnosti sa nazivom

--- public/routes/activity.js
+++ public/routes/activity.js
@@ -17,13 +17,12 @@ class Aktivnost {
         let data = fs.readFileSync('aktivnosti.txt', 'utf8');
 
         let splitted = data.toString().split("\n");
-        for (let i = 0; i<splitted.length; i++) {
+        for (let i = 0; i<splitted.length -1; i++) { //dodala sam ovdje -1
             let splitLine = splitted[i].split(",");
 
             let aktivnost = new Aktivnost(splitLine[0], splitLine[1],splitLine[2], splitLine[3], splitLine[4]);
             result.push(aktivnost);
         }
-        console.log(result);
         return result;
     }
 
--- public/routes/aktivnosti.js
+++ public/routes/aktivnosti.js
@@ -14,8 +14,8 @@ router.get('/', (req, res) => {
 
 router.post('/',function(req,res){
     let tijelo = req.body;
-    let novaLinija = "\n" + tijelo['naziv'] + "," + tijelo['tip'] + ","+
-                    tijelo['pocetak'] + "," + tijelo['kraj'] + "," + tijelo['dan'];
+    let novaLinija = tijelo['naziv'] + "," + tijelo['tip'] + ","+
+                    tijelo['pocetak'] + "," + tijelo['kraj'] + "," + tijelo['dan'] + "\n";
 
     let akt = new Aktivnost(tijelo['naziv'], tijelo['tip'], tijelo['pocetak'], tijelo['kraj'], tijelo['dan']);
     if(akt.timeValidation()) {
@@ -26,6 +26,31 @@ router.post('/',function(req,res){
     } else {
         res.json({message: "Aktivnost nije validna!", data: novaLinija});
     }
+});
+
+router.delete('/:id', (req, res) => {
+    let idParameter = req.params.id;
+    let akt = new Aktivnost();
+    let result = akt.readActivitiesFromFile();
+
+    let filtered = result.filter(element => element["naziv"] !== idParameter.toString() && element["naziv"]!=="\n");
+
+    fs.unlink('aktivnosti.txt', (err) => {
+        if (err) throw err;
+    });
+
+    function writeNewDataToFile(i) {
+        let novaLinija = filtered[i]["naziv"] + "," + filtered[i]["tip"] + "," +
+            filtered[i]["pocetak"] + "," + filtered[i]["kraj"] + "," + filtered[i]["dan"] + "\n";
+        fs.appendFile('aktivnosti.txt', novaLinija, function (err) {
+            if (err) throw err;
+        });
+    }
+
+    for(let i = 0; i < filtered.length; i++) {
+        writeNewDataToFile(i);
+    }
+    res.send(JSON.stringify(filtered));
 });
 
 module.exports = router;
