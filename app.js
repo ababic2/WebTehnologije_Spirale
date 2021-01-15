94acc60d7df6aeb0bdade0d2fa26ddc8d53d0c4d
Amina Babic <ababic2@etf.unsa.ba>
2021-01-15 15:15:44 +0100

    kreirani get requests

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const predmeti = require('./public/routes/predmeti');
 const aktivnosti = require('./public/routes/aktivnosti');
 const app = express();
 let activity = require('./public/routes/aktivnostClass');
+
+const db = require(__dirname+'/db/init.js');
+
 let Aktivnost = activity.Aktivnost;
 
 var cors = require('cors')
@@ -40,4 +43,110 @@ app.get('/v1/aktivnosti', (req, res) => {
     res.json(result);
 });
 
-app.listen(3000);
+app.get('/v2/aktivnost',(req,res) => {
+    db.Aktivnost.findAll().then(
+        (aktivnosti) => {
+            aktivnosti.map(aktivnost => {
+                return {
+                    id:aktivnost.id,
+                    naziv:aktivnost.naziv,
+                    pocetak:aktivnost.pocetak,
+                    kraj:aktivnost.kraj
+                }
+            })
+            res.json({
+                aktivnosti:aktivnosti
+            })
+        }
+    )
+});
+app.get('/v2/dan',(req,res) => {
+    db.Dan.findAll().then(
+        (dani) => {
+            dani.map(dan => {
+                return {
+                    id:dan.id,
+                    naziv:dan.naziv
+                }
+            })
+            res.json({
+                dani:dani
+            })
+        }
+    )
+});
+app.get('/v2/grupa',(req,res) => {
+    db.Grupa.findAll().then(
+        (grupe) => {
+            grupe.map(grupa => {
+                return {
+                    id:grupa.id,
+                    naziv:grupa.naziv
+                }
+            })
+            res.json({
+                grupe:grupe
+            })
+        }
+    )
+});
+app.get('/v2/predmet',(req,res) => {
+    db.Predmet.findAll().then(
+        (predmeti) => {
+            predmeti.map(predmet => {
+                return {
+                    id:predmet.id,
+                    naziv:predmet.naziv
+                }
+            })
+            res.json({
+                predmeti:predmeti
+            })
+        }
+    )
+});
+app.get('/v2/student',(req,res) => {
+    db.Student.findAll().then(
+        (studenti) => {
+            studenti.map(student => {
+                return {
+                    id:student.id,
+                    ime:student.ime,
+                    indeks:student.indeks
+                }
+            })
+            res.json({
+                studenti:studenti
+            })
+        }
+    )
+});
+app.get('/v2/tip',(req,res) => {
+    db.Tip.findAll().then(
+        (tipovi) => {
+            tipovi.map(tip => {
+                return {
+                    id:tip.id,
+                    naziv:tip.naziv,
+                    pocetak:tip.pocetak,
+                    kraj:tip.kraj
+                }
+            })
+            res.json({
+                tipovi:tipovi
+            })
+        }
+    )
+});
+
+db.init().then(
+    () => {
+        console.log('Listening on port 3000 after database initialization...')
+        app.listen(3000);
+        app.emit('start');
+    }
+)
+
+module.exports = app;
+
+
--- models/Aktivnost.js
+++ models/Aktivnost.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize');
 module.exports = function(sequelize,DataTypes){
     const Aktivnost = sequelize.define('Aktivnost',{
+        aktivnost: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true },
         naziv: Sequelize.STRING,
         pocetak: Sequelize.FLOAT,
         kraj: Sequelize.FLOAT
